3a140bf4652a798838ed3279a8b9f9edae0948ee
inakiDiaz <inakidiaz2808@gmail.com>
2025-01-23 12:25:11 -0300

    apartado contacto

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { ContactoService } from 'src/app/services/contacto.service'; // Importa el servicio
 
 @Component({
   selector: 'app-contacto',
@@ -8,10 +9,25 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class ContactoComponent {
 
-  constructor(private toastr: ToastrService) {}
+  constructor(private toastr: ToastrService,
+    private contactoService: ContactoService
+  ) {}
 
   enviarFormulario(form: any) {
-    form.reset(); 
-    this.toastr.success('¡Tu mensaje ha sido enviado con éxito!', 'Formulario Enviado');
+    if (form.valid) {
+      // Enviar datos al servicio
+      this.contactoService.enviarFormulario(form.value).subscribe(
+        (response) => {
+          form.reset();
+          this.toastr.success('¡Tu mensaje ha sido enviado con éxito!', 'Formulario Enviado');
+        },
+        (error) => {
+          console.error(error);
+          this.toastr.error('Hubo un error al enviar el formulario. Inténtalo nuevamente.', 'Error');
+        }
+      );
+    } else {
+      this.toastr.warning('Por favor, completa todos los campos requeridos.', 'Formulario Incompleto');
+    }
   }
 }
